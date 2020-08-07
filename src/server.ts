c83b72fb89a52d49fecb41cac462286c8d1011a5
Eduardo Oliva <eduardok07@hotmail.com>
2020-08-06 21:22:11 -0300

    Adding knex and sqlite3

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,10 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
+app.get('/', (request, response) => {
+  return response.json({ok: true})
+})
+
 app.listen(3333);
