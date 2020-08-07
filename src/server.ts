3beda2736cc9b165033ff514b28eba3de47186e4
Eduardo Oliva <eduardok07@hotmail.com>
2020-08-07 20:25:24 -0300

    Finishing routes

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from 'express';
+import routes from './routes';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors())
+
 app.use(express.json());
 
-app.get('/', (request, response) => {
-  return response.json({ok: true})
-})
+app.use(routes);
 
 app.listen(3333);
